945e9d7e40658923ada9a8bfe44b3dde7b690dfe
AadityaMukherjee2007 <aaditya.mukherjee2007@gmail.com>
2022-05-29 11:19:46 +0530

    8th commit

--- patternPrinting_number_1.java
+++ patternPrinting_number_1.java
@@ -9,38 +9,12 @@ public class patternPrinting_number_1
         
         for (int i = 1; i <= n; i++)
         {
-            for (int j = 1; j <= n - i; j++)
-            {
-                System.out.print(" ");
-            }
-            for (int k = 1; k <= i; k++)
-            {
-                if (k == 1)
-                {
-                    System.out.print(k);
-                }
-                else
-                {
-                    System.out.print(" ");
-                }
-            }
-            if (i == 1)
-            {
-                System.out.println();
-                continue;
-            }
-            for (int l = i - 1 ; l >= 1; l--)
-            {
-                if (l == 1)
-                {
-                    System.out.print(l);
-                }
-                else
-                {
-                    System.out.print(" ");
-                }
-            }
-            System.out.println();
-        }
+		for (int j = 1; j <= i; j++)
+		{
+			System.out.print(j);
+		}
+		System.out.println();
+	}
+          
     }
 }
