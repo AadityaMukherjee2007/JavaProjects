e9e46f1f00b0decf1f5013e8dbedf3c27bf0afa3
AadityaMukherjee2007 <84586322+AadityaMukherjee2007@users.noreply.github.com>
2022-08-12 21:13:33 +0530

    Add files via upload

--- array/search/BinarySearch.java
+++ array/search/BinarySearch.java
@@ -5,14 +5,32 @@ public class BinarySearch
     public boolean search(int[] arr, int n)
     {
         arr = new SelectionSort().ascending(arr);
+        int beg = arr[0], end = arr[arr.length - 1], pos = 0;
+        while (beg <= end)
+        {
+			int mid = (int) ((beg + end) / 2);
+			if (n == mid)
+			{
+				pos = mid;
+				return true;
+			}
+			else if (n > mid)
+			{
+				beg = mid;
+			}
+			else
+			{
+				end = mid;
+			}
+		}
         return false;
     }
-    
+    /*
     public boolean search(double[] arr, double n)
     {
         arr = new SelectionSort().ascending(arr);
         return false;
-    }
+    }*/
     
     public static void main(String args[])
     {
