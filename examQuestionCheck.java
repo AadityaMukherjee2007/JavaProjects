823157ce35fc4ccb13f0d14bc1960844024ac66a
AadityaMukherjee2007 <aaditya.mukherjee2007@gmail.com>
2022-10-12 22:49:10 +0530

    32nd commit

---
+++ examQuestionCheck.java
@@ -0,0 +1,14 @@
+public class examQuestionCheck
+{
+    public static void main(String args[])
+    {
+        for (int i = 1; i <= 3; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print(""+j+i+"");
+            }
+            System.out.println();
+        }
+    }
+}
