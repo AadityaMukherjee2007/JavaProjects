7d0fcb684f24ab68eee3dbf9046874e99f179ff7
AadityaMukherjee2007 <84586322+AadityaMukherjee2007@users.noreply.github.com>
2023-12-12 13:47:27 +0000

    41st commit

---
+++ WordCombination.java
@@ -0,0 +1,34 @@
+//print all possible combinations of a user inputted word.
+import java.util.Scanner;
+public class WordCombination
+{
+    public void com(String s)
+    {
+        int n = s.length();
+        for (int i = 0; i < n; i++)
+        {
+            String word = s.charAt(i) + s.substring(0, i) + s.substring(i + 1);
+            System.out.println(word);
+        }
+    }
+    // code note yet complete
+    public void combination(String s)
+    {
+        int n = s.length();
+        com(s);
+        for (int i = 0; i < n; i++)
+        {
+            for (int )
+        }
+    }
+
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Word: ");
+        String str = sc.next();
+
+        new WordCombination().combination(str);
+        sc.close();
+    }
+}
